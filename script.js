6d9e54df8b0dad45b2718f4045851afa2e683d90
devmahin <mahinhowlader123@gmail.com>
2023-11-16 22:08:37 +0600

    pagination

--- script.js
+++ script.js
@@ -37,17 +37,15 @@ function SaveInfo(userArray){
 function showDisplay(){
     let stateMent = ""
     userArray.forEach((value,index) => {
-        console.log(value)
         stateMent += `
-                    <tr>
+                    <tr class="tableData">
                     <th scope="row">${index+1}</th>
-                    <td>${value.name}</td>
+                    <td class="name">${value.name}</td>
                     <td class="d-flex justify-content-around">
                     <i class="bi bi-pencil-square"  onclick="EditTodo(${index})" ></i> 
                     <i class="bi bi-trash-fill" onclick="deleteTodo(${index})" ></i>
                     </td>
                     </tr>
-      
         `
         alltodo.innerHTML = stateMent
     })
@@ -64,3 +62,58 @@ function deleteTodo(id){
     userArray.splice(id,1)
     SaveInfo(userArray)
 }
+const searchInput = document.getElementById("searchInput")
+const td = document.querySelectorAll("#alltodo tr");
+searchInput.addEventListener("input", (e) => {
+    let inputVal = e.target.value.toLowerCase()
+    alltodo.innerHTML = "";
+    td.forEach((val,index) => {
+        const tr_td = val.querySelectorAll("td")
+        if(tr_td[0].innerText.toLocaleLowerCase().indexOf(inputVal) > -1){
+            alltodo.appendChild(val)
+        }
+        })
+        if(alltodo.innerHTML === ""){
+            alltodo.innerHTML = "Record is not found"
+        }
+})
+
+
+
+
+///pagination
+const pagenation = document.querySelector("#pagenation")
+const paginationalltodo = document.querySelectorAll("#alltodo tr")
+
+let record_per_page = 5;
+let pageNum = 1;
+let total_record_length = paginationalltodo.length
+let total_page = Math.ceil(total_record_length / record_per_page)
+
+genaretPage()
+pageRecord()
+    function pageRecord(){
+        let start_index = (pageNum - 1 ) * record_per_page;
+        let end_index = start_index + (record_per_page - 1);
+        let stateMent = "";
+        for(i=start_index; i<= end_index; i++){
+            stateMent += `<tr>${paginationalltodo[i].innerHTML}</tr>`
+        }
+        alltodo.innerHTML = stateMent
+    }
+
+    function genaretPage (){
+        // let prevBtn = `<li class="page-item" id="prevBtn"><a class="page-link" onclick="prevBtn()" href="#">Previous</a></li>`
+        // let nextBtn =  `<li class="page-item" id="nextBtn"><a class="page-link" onclick="nextBtn()" href="#">Next</a></li>`
+        // buttons += `<li class="page-item daynamc ${activeClass}" id="page${i}"><a class="page-link" href="#">${i}</a></li>`
+       
+        // pagenation.innerHTML = `${prevBtn}, ${buttons}, ${nextBtn}`
+    }
+    // function nextBtn (){
+    //     pageNum++;
+    //     pageRecord()
+    // }
+    // function prevBtn (){
+    //     pageNum--;
+    //     pageRecord()
+    // }
